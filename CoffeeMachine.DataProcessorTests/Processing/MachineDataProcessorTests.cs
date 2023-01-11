5870b21d0531714a7e69894ffbcd31ea5ad30cdc
fotini <f.batzoni@gmail.com>
2023-01-11 16:50:00 +0200

    Arrage and Act for ShouldSaveCountPerCoffeeType() unit test

---
+++ CoffeeMachine.DataProcessorTests/Processing/MachineDataProcessorTests.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using WiredBrainCoffee.DataProcessor.Model;
+
+namespace WiredBrainCoffee.DataProcessor.Processing;
+
+public class MachineDataProcessorTests
+{
+    [Fact]
+    public void ShouldSaveCountPerCoffeeType()
+    {
+        //Arrange
+        var machineDataProcessor = new MachineDataProcessor();
+        var items = new[]
+        {
+          new MachineDataItem("Cappuccino",new DateTime(2022,10,27,8,0,0)),
+          new MachineDataItem("Cappuccino",new DateTime(2022,10,27,9,0,0)),
+          new MachineDataItem("Espresso",new DateTime(2022,10,27,10,0,0))
+        };
+
+        //Act
+        machineDataProcessor.ProcessItems(items);
+
+        //Assert
+
+    }
+}
